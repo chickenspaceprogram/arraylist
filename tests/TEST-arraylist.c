3e59c27db37ec29eff66a52644c41f15d3489321
Athena Boose <pestpestthechicken@yahoo.com>
2025-02-09 21:07:40 -0800

    fixed another problem. i may be stupid

--- tests/TEST-arraylist.c
+++ tests/TEST-arraylist.c
@@ -15,7 +15,7 @@ int main(void) {
     char str[] = "hello world this is a test of an arraylist";
     char temp;
     size_t len = strlen(str);
-    for (size_t = 0; i < len; ++i) {
+    for (size_t i = 0; i < len; ++i) {
         assert(CSP_array_list_push(&list, &(str[i])) != -1);
     }
 
